3a1f06b66ef3bb2a3fe4d53d18b7f455ccb8795c
Harrison Pedro <hpedro1195@gmail.com>
2017-11-20 23:13:13 -0200

    Crete parser method, but need to fix an error 'token: RPARAN())'

--- lispf_ck.py
+++ lispf_ck.py
@@ -8,8 +8,18 @@ def ast(lispf_data):
 
     lexer = ox.make_lexer([
         ('NUMBER', r'\d+'),
-        ('NAME', r'[a-zA-Z]+'),
+        ('NAME', r'[-a-zA-Z]+'),
         ('LPARAN', r'[(]'),
         ('RPARAN', r'[)]'),
+        ('COMMENT', r';.*'),
+        ('NEWLINE', r'\s+'),
     ])
-    tokens_list = ['NUMBER', 'NAME', 'LPARAN', 'RPARAN', 'COMMENT', 'NEWLINE']
+    tokens_list = ['NAME', 'NUMBER', 'LPARAN', 'RPARAN']
+    parser = ox.make_parser([
+        ('stmt : LPARAN expr RPARAN', lambda x,y,z: y),
+        ('expr : term expr', lambda x,y: (x,) + y),
+        ('expr : term', lambda x: (x,)),
+        ('term : stmt', lambda x: x),
+        ('term : NUMBER', lambda x: x),
+        ('term : NAME', lambda x: x),
+    ], tokens_list)
